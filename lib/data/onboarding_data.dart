136f9b6d44bc2f7d4e277e62c77d5c2f901b41ad
Anjali R S <anjalirstvm@gmail.com>
2025-08-10 12:53:40 +0530

    added assets

--- lib/data/onboarding_data.dart
+++ lib/data/onboarding_data.dart
@@ -3,15 +3,15 @@ import '../models/onboarding_model.dart';
 final List<OnboardingModel> onboardingData = [
   OnboardingModel(
     title: 'Fresh & Hot Meals',
-    description: 'Enjoy freshly prepared meals served at your college canteen.',image:''
+    description: 'Enjoy freshly prepared meals served at your college canteen.',image:'assets/images/Picture1.png'
   ),
   OnboardingModel(
-    image: 'assets/images/food2.png',
+    image: 'assets/images/Picture2.png',
     title: 'Daily Menu & Specials',
     description:"Browse todays menu and see what's cooking .",
   ),
   OnboardingModel(
-    image: 'assets/images/food3.png',
+    image: 'assets/images/Picture3.png',
     title: 'Quick Pay & Pickup',
     description: 'Skip the queue - order and pay directly from your phone.',
   ),
